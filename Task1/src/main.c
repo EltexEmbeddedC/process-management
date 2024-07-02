fc88852720656f4e13d1b5aa7d77d746e42ceda7
Alexey-Sagaydak <alex230703@yandex.ru>
2024-07-03 01:28:33 +0700

    add task 1 and 2, update readme

--- Task1/src/main.c
+++ Task1/src/main.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 int main() {
+  pid_t pid, ppid, ret_id;
+  int exit_status;
+
+  ret_id = fork();
+
+  if (ret_id == 0)
+    printf("I'm a child, ");
+  else
+    printf("I'm a parent, ");
+
+  pid = getpid();
+  ppid = getppid();
+
+  printf("my pid = %d, ppid = %d\n", pid, ppid);
+
+  if (ret_id == 0) {
+    exit(EXIT_SUCCESS);
+  } else {
+    ret_id = wait(&exit_status);
+    printf("%d died with code %d\n", ret_id, exit_status);
+  }
+
   return 0;
 }
--- Task2/src/main.c
+++ Task2/src/main.c
@@ -1,5 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void print_process_info(int number) {
+  printf("process%d: pid = %d, ppid = %d\n", number, getpid(), getppid());
+}
 
 int main() {
+  pid_t ret_pid;
+  int exit_status;
+  print_process_info(0);
+
+  if (fork() == 0) {
+    print_process_info(1);
+
+    if (fork() == 0) {
+      print_process_info(3);
+      exit(3);
+    }
+
+    if (fork() == 0) {
+      print_process_info(4);
+      exit(4);
+    }
+
+    ret_pid = wait(&exit_status);
+    printf("message from 1: process%d died (pid = %d)\n",
+           WEXITSTATUS(exit_status), ret_pid);
+
+    ret_pid = wait(&exit_status);
+    printf("message from 1: process%d died (pid = %d)\n",
+           WEXITSTATUS(exit_status), ret_pid);
+
+    exit(1);
+  }
+
+  if (fork() == 0) {
+    print_process_info(2);
+
+    if (fork() == 0) {
+      print_process_info(5);
+      exit(5);
+    }
+
+    ret_pid = wait(&exit_status);
+    printf("message from 2: process%d died (pid = %d)\n",
+           WEXITSTATUS(exit_status), ret_pid);
+
+    exit(2);
+  }
+
+  ret_pid = wait(&exit_status);
+  printf("message from 0: process%d died (pid = %d)\n",
+         WEXITSTATUS(exit_status), ret_pid);
+
+  ret_pid = wait(&exit_status);
+  printf("message from 0: process%d died (pid = %d)\n",
+         WEXITSTATUS(exit_status), ret_pid);
+
   return 0;
 }
